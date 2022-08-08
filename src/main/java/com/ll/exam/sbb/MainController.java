f7bfa9d45cff6384136da214c9ec93dcc6dcfa1e
Young998904 <like0998904@gmail.com>
2022-08-08 23:52:50 +0900

    Sessoin

--- src/main/java/com/ll/exam/sbb/MainController.java
+++ src/main/java/com/ll/exam/sbb/MainController.java
@@ -3,12 +3,14 @@ package com.ll.exam.sbb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Controller
 public class MainController {
-    private int increaseNo = -1;
+    private int increaseNo = -1; // 서버 재시작되면 날라감
     @RequestMapping("/sbb")
     @ResponseBody
     public String index() {
@@ -91,5 +93,23 @@ public class MainController {
             case "장희성", "홍길동" -> "INFP";
             default -> "모름";
         };
+    }
+
+    @GetMapping("/saveSession/{name}/{value}")
+    @ResponseBody
+    public String saveSession(@PathVariable String name, @PathVariable String value, HttpServletRequest req) {
+        HttpSession session = req.getSession();
+
+        session.setAttribute(name, value);
+
+        return "세션변수 %s의 값이 %s(으)로 설정되었습니다.".formatted(name, value);
+    }
+
+    @GetMapping("/getSession/{name}")
+    @ResponseBody
+    public String getSession(@PathVariable String name, HttpSession session) {
+        String value = (String) session.getAttribute(name);
+
+        return "세션변수 %s의 값이 %s 입니다.".formatted(name, value);
     }
 }
