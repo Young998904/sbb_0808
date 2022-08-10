3f89dfe30196c163a69650481b807a98d9ea9f77
Young998904 <like0998904@gmail.com>
2022-08-10 14:32:09 +0900

    테스트 케이스 추가 : findBySubjectAndContent()

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
 }
