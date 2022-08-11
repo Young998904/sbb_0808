df7674529358d6533a92f843db40ba51b56c43cd
Young998904 <like0998904@gmail.com>
2022-08-11 13:31:13 +0900

    테스트 케이스 추가 : testJpa0 trucate

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -2,8 +2,11 @@ package com.ll.exam.sbb;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -13,4 +16,28 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String subject);
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "truncate question",
+            nativeQuery = true
+    )
+    void truncate();
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "SET FOREIGN_KEY_CHECKS = 0",
+            nativeQuery = true
+    )
+    void disableForeignKeyChecks();;
+
+    @Transactional
+    @Modifying
+    @Query(
+            value = "SET FOREIGN_KEY_CHECKS = 1",
+            nativeQuery = true
+    )
+    void enableForeignKeyChecks();
 }
